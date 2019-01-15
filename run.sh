e050a7d668fd8087d7ddde2298ef9d9556538d6a
Nick Hamilton <github@deeplucid.xyx>
2019-01-16 09:36:05 +1000

    Initial setup.

--- run.sh
+++
@@ -1,6 +0,0 @@
-read -p "Unplug right keyboard, press flash button on left keyboard."
-make ergodox_infinity:nicksos
-sudo make ergodox_infinity:nicksos:dfu-util
---make ergodox_infinity:nicksos MASTER=right
---read -p "Unplug the left keyboard, plug in the right keyboard. Press the flash button on the right keyboard."
---sudo make ergodox_infinity:nicksos:dfu-util MASTER=right
